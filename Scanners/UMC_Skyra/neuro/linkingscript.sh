f8e015982971ee07c070f413cd9ce59f56ca28e4
msveiven <sveiven@email.arizona.edu>
2017-01-17 21:17:02 -0700

    first commit

--- Scanners/UMC_Skyra/neuro/linkingscript.sh
+++ Scanners/UMC_Skyra/neuro/linkingscript.sh
@@ -6,10 +6,19 @@ touch index.htm
 
 echo "<DOCTYPE! html>" > index.htm
 
-for file in *.htm; do echo "<p>
-<A HREF = "https://msveiven.github.io/Test/Scanners/UMC_Skyra/neuro/"$file"">"$file"</A>
+for file in *.htm; 
+do
+
+
+
+if ("$file" != "index.htm")
+then 
+echo "<p>
+<A HREF = "https://msveiven.github.io/Test/Scanners/UMC_Skyra/neuro/$file">"$file"</A>
 </p> 
-" >> index.htm; done
+" >> index.htm
+fi; 
+done
  
  
 
