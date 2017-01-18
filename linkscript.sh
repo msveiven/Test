9bb0c4e643fb9787fb3ccd7d202001a48d1dcaa5
msveiven <sveiven@email.arizona.edu>
2017-01-18 10:37:44 -0700

    first commit

--- linkscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-touch index.htm
-PATH1 = pwd.split('GitHub',1)
-for file in */!(index.htm); do echo "<p>
-<A HREF = "https://msveiven.github.io/$PATH1/$file">$file</A>
-</p> 
-" > index.htm; done
-
-
